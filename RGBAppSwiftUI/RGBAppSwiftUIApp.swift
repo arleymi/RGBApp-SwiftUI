4ee9fd7a76c7a15a93355c0d9ad2863f719f7ac8
rleymi <archerlv99@gmail.com>
2021-09-10 14:49:36 +0600

    Initial Commit

---
+++ RGBAppSwiftUI/RGBAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppSwiftUIApp.swift
+//  RGBAppSwiftUI
+//
+//  Created by Arthur Lee on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
