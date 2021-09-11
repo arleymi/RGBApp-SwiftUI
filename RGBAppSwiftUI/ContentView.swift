13cb83dca9bd9a2df8c7743fd20e144de698048f
rleymi <archerlv99@gmail.com>
2021-09-11 17:33:40 +0600

    committing alert for numbers

--- RGBAppSwiftUI/ContentView.swift
+++ RGBAppSwiftUI/ContentView.swift
@@ -8,14 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.gray.opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+            VStack() {
+                ColorView(redValue: redSliderValue/255, greenValue: greenSliderValue/255, blueValue: blueSliderValue/255)
+                ValueSettingsStack(value: $redSliderValue, colorSlider: .red)
+                ValueSettingsStack(value: $greenSliderValue, colorSlider: .green)
+                ValueSettingsStack(value: $blueSliderValue, colorSlider: .blue)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ValueSettingsStack: View {
+   
+    @Binding var value: Double
+    @State private var stringEnteredValue = ""
+    @State private var alertPresented = false
+    let colorSlider: Color
+    
+    
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .frame(width: 50)
+            Slider(value: $value, in: 0...255, step: 1)
+                    .accentColor(colorSlider)
+                .frame(width: 250)
+            TextField("", value: $value, formatter: NumberFormatter(), onCommit:
+                       {
+                        filterTextfield()
+                       })
+                .alert(isPresented: $alertPresented, content: {
+                    Alert(title: Text("Wrong Format!"), message: Text("Try again!"))
+                })
+                .keyboardType(.numbersAndPunctuation)
+                .multilineTextAlignment(.center)
+                .frame(width: 50)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+        }
+       
+    }
+    
+
+    private func filterTextfield() {
+
+        
+        switch value {
+        case ..<0:
+            self.value = 0; alertPresented.toggle()
+        case 0...255:
+            break
+        default:
+            self.value = 255; alertPresented.toggle()
+        }
+    }
+}
+
+
+struct ColorView: View {
+    
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+    
+    var body: some View {
+        Color(red: redValue, green: greenValue, blue: blueValue)
+            .frame(width: 360, height: 200)
+            .cornerRadius(20)
+            .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 4))
     }
 }
